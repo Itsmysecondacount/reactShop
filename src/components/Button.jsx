a680b7ba792fde4a86de01f408618831918da78a
Jorge Castillo <jorge.castillo.torres.a@gmail.com>
2023-03-02 09:21:51 -0500

    Subiendo todos los edits

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Button = () => (
+	<>
+		<div>
+			<h1>Hello</h1>
+		</div>
+	</>
+);
---
+++ src/routes/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Route, Routes, HashRouter } from 'react-router-dom';
+import Layout from '../containers/Layout';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import PasswordRecovery from '../pages/PasswordRecovery';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
+import NotFound from '../pages/NotFound';
+import '../styles/global.css';
+import { Button } from '../components/Button';
+
+const App = () => {
+	return (
+		<HashRouter>
+			<Layout>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/password-recovery" element={<PasswordRecovery />} />
+					<Route path="/send-email" element={<SendEmail />} />
+					<Route path="/new-password" element={<NewPassword />} />
+					<Route path="/account" element={<MyAccount />} />
+					<Route path="/signup" element={<CreateAccount />} />
+					<Route path="/checkout" element={<Checkout />} />
+					<Route path="/orders" element={<Orders />} />
+					<Route path="/pruebas" element={<Button />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Layout>
+		</HashRouter>
+	);
+};
+
+export default App;
