6f86574d3c126d304422532211de6f6573a96059
Jorge Castillo <jorge.castillo.torres.a@gmail.com>
2023-03-02 12:54:02 -0500

    add @ for all of imports (shortcuts)

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -13,26 +13,31 @@ import Orders from '../pages/Orders';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
 import { Button } from '../components/Button';
+import { AppContext } from '../context/AppContext';
+import { useInitialState } from '../hooks/useInitialState';
 
 const App = () => {
+	const initialState = useInitialState();
 	return (
-		<HashRouter>
-			<Layout>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/password-recovery" element={<PasswordRecovery />} />
-					<Route path="/send-email" element={<SendEmail />} />
-					<Route path="/new-password" element={<NewPassword />} />
-					<Route path="/account" element={<MyAccount />} />
-					<Route path="/signup" element={<CreateAccount />} />
-					<Route path="/checkout" element={<Checkout />} />
-					<Route path="/orders" element={<Orders />} />
-					<Route path="/pruebas" element={<Button />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</Layout>
-		</HashRouter>
+		<AppContext.Provider value={initialState}>
+			<HashRouter>
+				<Layout>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/password-recovery" element={<PasswordRecovery />} />
+						<Route path="/send-email" element={<SendEmail />} />
+						<Route path="/new-password" element={<NewPassword />} />
+						<Route path="/account" element={<MyAccount />} />
+						<Route path="/signup" element={<CreateAccount />} />
+						<Route path="/checkout" element={<Checkout />} />
+						<Route path="/orders" element={<Orders />} />
+						<Route path="/pruebas" element={<Button />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</Layout>
+			</HashRouter>
+		</AppContext.Provider>
 	);
 };
 
